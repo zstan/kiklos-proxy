8afa25861a9cb57e43fcbc20bfeb01dd4137a492
Stanilovsky Evgeny <zstan@yandex-team.ru>
2014-04-18 09:26:38 +0400

    fix

--- kiklos-core/src/main/java/org/amrmostafa/experiments/netty/MemoryLogStorage.java
+++ kiklos-core/src/main/java/org/amrmostafa/experiments/netty/MemoryLogStorage.java
@@ -1,17 +1,19 @@
 package org.amrmostafa.experiments.netty;
 
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
 
 import org.redisson.Redisson;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class MemoryLogStorage {
 	
 	private final List<String> memStorageList;	
 	private List<String> logData = new ArrayList<String>();
-	private final int LOG_DATA_SIZE = 100;
+	private final int LOG_DATA_SIZE = 5; // !!!!
 	private static final String LOG_NAME = ".access_log";
+	private static final Logger LOG = LoggerFactory.getLogger(MemoryLogStorage.class);
 	
 	MemoryLogStorage(final Redisson memStorage) {
 		memStorageList = memStorage.getList(LOG_NAME);
@@ -19,9 +21,12 @@ public class MemoryLogStorage {
 	
 	public synchronized void put(final String str) {
 		logData.add(str);
-		if (logData.size() == LOG_DATA_SIZE)
+		if (logData.size() == LOG_DATA_SIZE) {
 			memStorageList.addAll(logData);
-		logData.clear();
+			LOG.info("MemoryLogStorage put data");
+			System.out.println("MemoryLogStorage put data");
+			logData.clear();
+		}		
 	}
 
 }
