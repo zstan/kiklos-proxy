357371612362f3efe8e79a0f731d7b6932d2ef82
Stanilovsky Evgeny <zstan@yandex-team.ru>
2014-05-08 13:47:25 +0400

    fix placements

--- fill_placements.sh
+++ fill_placements.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
-redis-cli HSET "\".placements\"" "\"111\"" "[\"java.util.ArrayList\",[\"2504637\",\"some comment1\"]]"
-redis-cli HSET "\".placements\"" "\"112\"" "[\"java.util.ArrayList\",[\"2504635\",\"some comment2\"]]"
-redis-cli HSET "\".placements\"" "\"222\"" "[\"java.util.ArrayList\",[\"2504636\",\"some comment3\"]]"
+redis-cli HSET "\".placements\"" "\"111\"" "[\"java.util.ArrayList\",[\"http://ib.adnxs.com/ptv?id=2504637\",\"smartlabs\"]]"
+redis-cli HSET "\".placements\"" "\"112\"" "[\"java.util.ArrayList\",[\"http://ib.adnxs.com/ptv?id=2504637\",\"bcs\"]]"
+redis-cli HSET "\".placements\"" "\"113\"" "[\"java.util.ArrayList\",[\"http://asg.vidigital.ru/1/50006/c/v/2\",\"asg1\"]]"
+redis-cli HSET "\".placements\"" "\"114\"" "[\"java.util.ArrayList\",[\"http://ads.adfox.ru/216891/getCodeTest?p1=blhhs&p2=euhw&pfc=a&pfb=a&plp=a&pli=a&pop=a\",\"hz1\"]]"
